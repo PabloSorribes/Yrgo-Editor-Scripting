2ff48607e897a5dad7d32b34ef1ca428474af87a
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-05-04 01:09:34 +0200

    Adding ScriptableWizard_SelectAllWithTag Editor Tool

---
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_01 - Basics/ScriptableWizard_SelectAllWithTag/Scripts/Editor/ScriptableWizard_SelectAllWithTag.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ScriptableWizard_SelectAllWithTag : ScriptableWizard
+{
+	public string searchTag = "Your tag here";
+
+	[MenuItem("YRGO/Part 01/ScriptableWizard - Select All With Tag...")]
+	static void SelectAllWithTagWizard()
+	{
+		ScriptableWizard.DisplayWizard<ScriptableWizard_SelectAllWithTag>("Select All With Tag...", "Make Selection", "Some Other Button");
+	}
+
+	void OnWizardCreate()
+	{
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(searchTag);
+		Selection.objects = gameObjects;
+	}
+
+	private void OnWizardOtherButton()
+	{
+		Debug.Log($"Clicked other button!");
+	}
+
+	private void OnWizardUpdate()
+	{
+		//Debug.Log($"A value on the Wizard was updated.");
+	}
+}
