6561992767737e4b064722de48428c05b07e8f99
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-05-04 15:46:29 +0200

    Adding an "EditorPrefs"-folder and added that "TheKnightsOfUnity"-script for turning on/off cheats in the editor.

---
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_05 - Advanced & Misc/EditorPrefs/CheatsEditorWindow/Cheats.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright (c) The Knights of Unity
+ * http://theknightsofunity.com/
+ */
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Cheats
+{
+    public static bool MuteAllSounds
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return EditorPrefs.GetBool("MuteAllSounds", false);
+#else
+            return false;
+#endif
+        }
+
+        set
+        {
+#if UNITY_EDITOR
+            EditorPrefs.SetBool("MuteAllSounds", value);
+#endif
+        }
+    }
+
+    public static int PlayerLifes
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return EditorPrefs.GetInt("PlayerLifes", 3);
+#else
+            return false;
+#endif
+        }
+
+        set
+        {
+#if UNITY_EDITOR
+            EditorPrefs.SetInt("PlayerLifes", value);
+#endif
+        }
+    }
+
+    public static string PlayerTwoName
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return EditorPrefs.GetString("PlayerTwoName", "John");
+#else
+            return false;
+#endif
+        }
+
+        set
+        {
+#if UNITY_EDITOR
+            EditorPrefs.SetString("PlayerTwoName", value);
+#endif
+        }
+    }
+}
---
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_05 - Advanced & Misc/EditorPrefs/CheatsEditorWindow/Editor/CheatsWindow.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Copyright (c) The Knights of Unity
+ * http://theknightsofunity.com/
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+public class CheatsWindow : EditorWindow
+{
+    [MenuItem("Examples/The Knights of Unity/Cheats")]
+    public static void ShowWindow()
+    {
+        GetWindow<CheatsWindow>(false, "Cheats", true);
+    }
+
+    void OnGUI()
+    {
+        Cheats.MuteAllSounds = EditorGUILayout.Toggle("Mute All Sounds", Cheats.MuteAllSounds);
+        Cheats.PlayerLifes = EditorGUILayout.IntField("Player Lifes", Cheats.PlayerLifes);
+        Cheats.PlayerTwoName = EditorGUILayout.TextField("Player Two Name", Cheats.PlayerTwoName);
+
+        GUILayout.FlexibleSpace();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        GUI.backgroundColor = Color.red;
+
+        if (GUILayout.Button("Reset", GUILayout.Width(100), GUILayout.Height(30)))
+        {
+            Cheats.MuteAllSounds = false;
+            Cheats.PlayerLifes = 4;
+            Cheats.PlayerTwoName = "John";
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
