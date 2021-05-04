68fe8a75235a868de0cd4be1f8f031c8a7cb3b53
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-05-04 15:39:02 +0200

    Adding "ChangeObjectHeight (HowTo old Undo-system)"

---
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_05 - Advanced & Misc/ChangeObjectHeight (HowTo old Undo-system)/Scripts/ChangeObjectHeight.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class ChangeObjectHeight : MonoBehaviour
+{
+	public float minHeight = 0;
+	public float maxHeight = 5;
+}
---
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_05 - Advanced & Misc/ChangeObjectHeight (HowTo old Undo-system)/Scripts/Editor/ChangeObjectHeightEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ChangeObjectHeight))]
+public class ChangeObjectHeightEditor : Editor
+{
+	ChangeObjectHeight m_Target;
+
+	public override void OnInspectorGUI()
+	{
+		m_Target = (ChangeObjectHeight)target;
+
+		base.OnInspectorGUI();
+		DrawChangeHeightSlider();
+	}
+
+	private void DrawChangeHeightSlider()
+	{
+		Vector3 objectPos = m_Target.transform.position;
+		objectPos.y = EditorGUILayout.Slider(label: "Object's Height: ", objectPos.y, m_Target.minHeight, m_Target.maxHeight);
+
+		// Need to check if a change actually happened to avoid Recording an Undo-step each frame in OnInspectorGUI().
+		if (objectPos.y != m_Target.transform.position.y)
+		{
+			Undo.RecordObject(m_Target.transform, "Change Object's Height");
+			m_Target.transform.position = objectPos;
+		}
+	}
+}
