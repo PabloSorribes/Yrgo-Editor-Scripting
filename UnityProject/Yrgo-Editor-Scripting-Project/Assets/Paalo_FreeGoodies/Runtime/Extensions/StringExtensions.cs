6738bd14935be247d7ba9dc9b651fac09071979c
PabloSorribes <pablo.sorribes.bernhard@gmail.com>
2021-05-04 16:38:07 +0200

    Adding more StringExtensions

--- UnityProject/Yrgo-Editor-Scripting-Project/Assets/Paalo_FreeGoodies/Runtime/Extensions/StringExtensions.cs
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/Paalo_FreeGoodies/Runtime/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
 
 namespace Paalo.UnityMiscTools.Extensions
 {
@@ -19,6 +22,45 @@ namespace Paalo.UnityMiscTools.Extensions
 			Debug.LogWarning($"There was no matching string '{subStringToRemove}' inside of '{self}'.\n" +
 				$"Will return '{self}' instead.");
 			return self;
+		}
+
+		// Source: https://stackoverflow.com/a/4405876
+		public static string CapitalizeFirstLetter(this string input)
+		{
+			switch (input)
+			{
+				case null: throw new ArgumentNullException(nameof(input));
+				case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+				default: return input.First().ToString().ToUpper() + input.Substring(1);
+			}
+		}
+
+		// Source: https://stackoverflow.com/a/4335913
+		public static string TrimStart(this string target, string trimString)
+		{
+			if (string.IsNullOrEmpty(trimString)) return target;
+
+			string result = target;
+			while (result.StartsWith(trimString))
+			{
+				result = result.Substring(trimString.Length);
+			}
+
+			return result;
+		}
+
+		// Source: https://stackoverflow.com/a/4335913
+		public static string TrimEnd(this string target, string trimString)
+		{
+			if (string.IsNullOrEmpty(trimString)) return target;
+
+			string result = target;
+			while (result.EndsWith(trimString))
+			{
+				result = result.Substring(0, result.Length - trimString.Length);
+			}
+
+			return result;
 		}
 	}
 }
