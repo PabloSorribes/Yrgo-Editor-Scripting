53c1dd5564f2cc40e927f050b880d77be7995b53
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2022-04-18 23:38:38 +0200

    PlayerController does some random field accessing

--- UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_05 - Advanced & Misc/SettingsOverviewWindow (Window hosting other Inspectors)/Scripts/Runtime/PlayerController.cs
+++ UnityProject/Yrgo-Editor-Scripting-Project/Assets/2021/Part_05 - Advanced & Misc/SettingsOverviewWindow (Window hosting other Inspectors)/Scripts/Runtime/PlayerController.cs
@@ -7,6 +7,10 @@ namespace SettingsOverviewWindowExample.Runtime
 {
 	public class PlayerController : MonoBehaviour
 	{
+		public SettingsOverviewWindowExample.Data.PlayerStats playerStats;
+
+		private int MyCalculatedInt => playerStats.myPlayerInt * 2;
+
 		public void UpdateStatsFromEditorWindow()
 		{
 			
